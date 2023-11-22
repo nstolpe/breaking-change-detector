2a6c6e590f4f81f0d198327243dcb65f93a811e8
Nils Stolpe <nils.stolpe@nearform.com>
2023-11-22 18:59:23 -0500

    made a breaking change

--- services/bora-horza-gobuchul/routes/root.js
+++ services/bora-horza-gobuchul/routes/root.js
@@ -4,8 +4,6 @@
 module.exports = async function (fastify, opts) {
   fastify.get('/', async (request, reply) => {
     const limitingFactorPayload = await request['limitingFactor'].get()
-    console.log(await request['limitingFactor'].get())
-    console.log('foobar', fastify.example)
     return { ...limitingFactorPayload, 'bora-horza-gobuchul': fastify.example }
   })
 }
